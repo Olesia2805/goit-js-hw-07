a19234bd484b6454ad3ce1ecb6167bc97eb4582a
Olesia2805 <vysotina2002@gmail.com>
2024-06-28 23:03:38 +0200

    ternary operator

--- js/task-3.js
+++ js/task-3.js
@@ -3,9 +3,5 @@ const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
   const trimValue = event.currentTarget.value.trim();
-  if (trimValue) {
-    output.textContent = trimValue;
-  } else {
-    output.textContent = "Anonymous";
-  }
+  output.textContent = trimValue === '' ? "Anonymous" : trimValue;
 });
