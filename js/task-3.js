a9e5eeb53bb7ac1e3dce1d1713cc4188005ebf89
Olesia2805 <vysotina2002@gmail.com>
2024-06-29 17:03:53 +0200

    use pretty

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
+const textInput = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
 
-textInput.addEventListener("input", (event) => {
+textInput.addEventListener('input', event => {
   const trimValue = event.currentTarget.value.trim();
-  output.textContent = trimValue === '' ? "Anonymous" : trimValue;
+  output.textContent = trimValue === '' ? 'Anonymous' : trimValue;
 });
