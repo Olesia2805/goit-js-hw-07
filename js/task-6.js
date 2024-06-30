52d228558733ae8d7e9cdf8c766d3dbf6f68fb6b
Olesia2805 <vysotina2002@gmail.com>
2024-07-01 00:39:07 +0200

    done task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function destroyBoxes() {
+  document.querySelector('#boxes').innerHTML = '';
+}
+
+function createBoxes(amount) {
+  let template = '';
+  let size = 30;
+
+  for (let index = 0; index < amount; index++) {
+    template += `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`;
+    size += 10;
+  }
+
+  document.querySelector('#boxes').innerHTML = template;
+}
+
+const addBtn = document.querySelector('button[data-create]');
+const delBtn = document.querySelector('button[data-destroy]');
+const amountInput = document.querySelector('#controls input');
+
+addBtn.addEventListener('click', () => {
+  let amount = parseInt(amountInput.value);
+
+  if (amount > 0 && amount <= 100) {
+    createBoxes(amount);
+    amountInput.value = '';
+  } else {
+    return alert('Amount must be between 1 and 100');
+  }
+});
+
+delBtn.addEventListener('click', destroyBoxes);
